24427243f83476695e31c213e0bb05a5db2ce51c
IN0C3NC10 <bruno.inocencio@fatec.sp.gov.br>
2021-09-01 11:39:49 -0300

    Dir structure and Firebase, Stack dependencies added

---
+++ src/config/firebaseconfig.js
@@ -0,0 +1,18 @@
+import firebase from "firebase"
+import "firebase/storage"
+
+const firebaseConfig = {
+    apiKey: "AIzaSyDVDkrbolTerjm1iDOItXj7ueOnPA7xGaw",
+    authDomain: "reactnative-688b7.firebaseapp.com",
+    projectId: "reactnative-688b7",
+    storageBucket: "reactnative-688b7.appspot.com",
+    messagingSenderId: "917486679648",
+    appId: "1:917486679648:web:3022ca074171f879bc3505"
+  };
+  
+  
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
+
+  const database = firebase.firestore()
+  export default database
